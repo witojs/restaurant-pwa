55b8d54ea7c2f9ef92fe54c31d4553ba67a92afa
witojs <116544540+witojs@users.noreply.github.com>
2023-08-23 22:02:21 +0700

    commenting bundleAnalyzer

--- restaurant-apps-wito-pwa-sub3-wito/webpack.common.js
+++ restaurant-apps-wito-pwa-sub3-wito/webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 const ImageminMozjpeg = require('imagemin-mozjpeg');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+/* const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin; */
 
 module.exports = {
   entry: {
@@ -80,6 +80,6 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    /* new BundleAnalyzerPlugin(), */
   ],
 };
